4aacec895241993d90ec91f25653d46fc268302a
Sergei Komarov <komarovs33@mail.ru>
2020-02-04 16:26:20 +0300

    init

---
+++ src/components/EditExpensePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EditExpensePage = props => {
+  console.log(props);
+
+  return <div>EditExpensePage</div>;
+};
+
+export default EditExpensePage;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  watch: true,
+  // entry: "./src/index.js",
+  entry: "./src/playground/redux-expensify.ts",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+};
